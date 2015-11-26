adc8966351b8a74067efa18d594775c297fba6c0
zhangyw <zhangyw_001@126.com>
2015-11-26 15:58:48 +0800

    20151126

--- mail4j/src/test/java/com/zhangyingwei/mail/MailLoginTest.java
+++ mail4j/src/test/java/com/zhangyingwei/mail/MailLoginTest.java
@@ -9,9 +9,9 @@ import com.zhangyingwei.model.MailMessage;
 
 public class MailLoginTest {
 	public static void main(String[] args) throws Exception {
-		String toaddress = "zhangyw_001@126.com";
-//		String toaddress = "1170404227@qq.com";
-		MailHandler maillogin = MailHandler.getInstence("zhangyw_001@163.com","723129bao");
+		String toaddress = "mail@mail.com";
+//		String toaddress = "mail@mail.com";
+		MailHandler maillogin = MailHandler.getInstence("mail@mail.com","password");
 		maillogin.sendMessage(new MailMessage(maillogin.getMail()).setToAddress(toaddress).setSubject("这是一个封测试邮件，我是邮件的标题").addText("这是一个邮件内容").addExtra("G:\\jilege.doc").init());
 	}
 }
